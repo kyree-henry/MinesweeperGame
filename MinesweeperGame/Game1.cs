58c66c631369f0a2644ec0450f0d7b8c4cc3f63c
Kyree henry <ene.henry.eh@gmail.com>
2025-05-08 14:07:21 +0100

    updated ui and added gameover functionality also added highScore and timer

--- MinesweeperGame/Game1.cs
+++ MinesweeperGame/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace MinesweeperGame
 {
@@ -9,13 +10,22 @@ namespace MinesweeperGame
         GraphicsDeviceManager _graphics;
         SpriteBatch _spriteBatch;
 
-        int rows = 10, cols = 10, cellSize = 40, mineCount = 15;
+        int rows = 16, cols = 30, cellSize = 35, mineCount = 70;
         bool[,] mines;
         bool[,] revealed;
         int[,] adjacentMines;
+
         SpriteFont font;
         Texture2D cellTexture;
+
         MouseState prevMouse;
+
+        bool gameOver = false;
+        bool win = false;
+        double elapsedTime = 0;
+        double highScore = double.MaxValue;
+        bool scoreRecorded = false;
+        Rectangle resetButtonRect;
 
         public Game1()
         {
@@ -23,17 +33,14 @@ namespace MinesweeperGame
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
 
-            _graphics.PreferredBackBufferWidth = 400;
-            _graphics.PreferredBackBufferHeight = 400;
+            _graphics.PreferredBackBufferWidth = cols * cellSize;
+            _graphics.PreferredBackBufferHeight = rows * cellSize + 40;
         }
 
         protected override void Initialize()
         {
-            mines = new bool[rows, cols];
-            revealed = new bool[rows, cols];
-            adjacentMines = new int[rows, cols];
-            PlaceMines();
-            CalculateAdjacents();
+            resetButtonRect = new Rectangle(10, rows * cellSize + 5, 80, 30);
+            ResetGame();
             base.Initialize();
         }
 
@@ -45,9 +52,22 @@ namespace MinesweeperGame
             cellTexture.SetData(new[] { Color.White });
         }
 
+        void ResetGame()
+        {
+            mines = new bool[rows, cols];
+            revealed = new bool[rows, cols];
+            adjacentMines = new int[rows, cols];
+            gameOver = false;
+            win = false;
+            elapsedTime = 0;
+            scoreRecorded = false;
+            PlaceMines();
+            CalculateAdjacents();
+        }
+
         void PlaceMines()
         {
-            var rand = new System.Random();
+            var rand = new Random();
             int placed = 0;
             while (placed < mineCount)
             {
@@ -77,25 +97,56 @@ namespace MinesweeperGame
 
         protected override void Update(GameTime gameTime)
         {
-            MouseState mouse = Mouse.GetState();
+            var mouse = Mouse.GetState();
 
-            if (mouse.LeftButton == ButtonState.Pressed && prevMouse.LeftButton == ButtonState.Released)
+            // Reset button logic
+            if (mouse.LeftButton == ButtonState.Pressed && prevMouse.LeftButton == ButtonState.Released &&
+                resetButtonRect.Contains(mouse.Position))
             {
-                int col = mouse.X / cellSize;
-                int row = mouse.Y / cellSize;
-                if (row >= 0 && row < rows && col >= 0 && col < cols)
+                ResetGame();
+            }
+
+            if (!gameOver)
+            {
+                elapsedTime += gameTime.ElapsedGameTime.TotalSeconds;
+
+                if (mouse.LeftButton == ButtonState.Pressed && prevMouse.LeftButton == ButtonState.Released)
                 {
-                    if (mines[row, col])
+                    int col = mouse.X / cellSize;
+                    int row = mouse.Y / cellSize;
+                    if (row >= 0 && row < rows && col >= 0 && col < cols)
                     {
-                        // Reveal all mines
-                        for (int r = 0; r < rows; r++)
-                            for (int c = 0; c < cols; c++)
-                                if (mines[r, c]) revealed[r, c] = true;
+                        if (mines[row, col])
+                        {
+                            // Reveal mines and end
+                            for (int r = 0; r < rows; r++)
+                                for (int c = 0; c < cols; c++)
+                                    if (mines[r, c]) revealed[r, c] = true;
+                            gameOver = true;
+                            win = false;
+                        }
+                        else
+                        {
+                            RevealCell(row, col);
+                            int count = 0;
+                            for (int r = 0; r < rows; r++)
+                                for (int c = 0; c < cols; c++)
+                                    if (revealed[r, c]) count++;
+                            if (count == rows * cols - mineCount)
+                            {
+                                gameOver = true;
+                                win = true;
+                            }
+                        }
                     }
-                    else
-                    {
-                        RevealCell(row, col);
-                    }
+                }
+
+                // Record high score when game ends
+                if (gameOver && !scoreRecorded)
+                {
+                    if (elapsedTime < highScore)
+                        highScore = elapsedTime;
+                    scoreRecorded = true;
                 }
             }
 
@@ -109,14 +160,10 @@ namespace MinesweeperGame
                 return;
 
             revealed[row, col] = true;
-
             if (adjacentMines[row, col] == 0)
-            {
                 for (int dr = -1; dr <= 1; dr++)
                     for (int dc = -1; dc <= 1; dc++)
-                        if (!(dr == 0 && dc == 0))
-                            RevealCell(row + dr, col + dc);
-            }
+                        if (!(dr == 0 && dc == 0)) RevealCell(row + dr, col + dc);
         }
 
         protected override void Draw(GameTime gameTime)
@@ -124,25 +171,44 @@ namespace MinesweeperGame
             GraphicsDevice.Clear(Color.CornflowerBlue);
             _spriteBatch.Begin();
 
+            // Draw grid
             for (int r = 0; r < rows; r++)
                 for (int c = 0; c < cols; c++)
                 {
-                    Rectangle rect = new Rectangle(c * cellSize, r * cellSize, cellSize - 2, cellSize - 2);
-                    Color color = revealed[r, c] ? Color.LightGray : Color.Gray;
+                    var rect = new Rectangle(c * cellSize, r * cellSize, cellSize - 2, cellSize - 2);
+                    var color = revealed[r, c] ? Color.LightGray : Color.Gray;
                     _spriteBatch.Draw(cellTexture, rect, color);
 
                     if (revealed[r, c])
                     {
                         if (mines[r, c])
-                        {
                             _spriteBatch.DrawString(font, "*", new Vector2(c * cellSize + 10, r * cellSize + 5), Color.Red);
-                        }
                         else if (adjacentMines[r, c] > 0)
-                        {
                             _spriteBatch.DrawString(font, adjacentMines[r, c].ToString(), new Vector2(c * cellSize + 10, r * cellSize + 5), Color.Black);
-                        }
                     }
                 }
+
+            // Draw UI background
+            var uiBg = new Rectangle(0, rows * cellSize, cols * cellSize, 40);
+            _spriteBatch.Draw(cellTexture, uiBg, Color.DarkSlateGray);
+
+            // Draw reset button and timers
+            _spriteBatch.Draw(cellTexture, resetButtonRect, Color.Bisque);
+            _spriteBatch.DrawString(font, "Reset", new Vector2(resetButtonRect.X + 10, resetButtonRect.Y + 7), Color.Black);
+            _spriteBatch.DrawString(font, $"Time: {elapsedTime:F1}s", new Vector2(100, rows * cellSize + 10), Color.White);
+            _spriteBatch.DrawString(font, highScore < double.MaxValue ? $"Best: {highScore:F1}s" : "Best: --",
+                new Vector2(250, rows * cellSize + 10), Color.White);
+
+            // Draw game over overlay
+            if (gameOver)
+            {
+                var overlay = new Rectangle(0, 0, cols * cellSize, rows * cellSize);
+                _spriteBatch.Draw(cellTexture, overlay, Color.Black * 0.5f);
+                string msg = win ? "You Win!" : "Game Over!";
+                var size = font.MeasureString(msg);
+                var pos = new Vector2((cols * cellSize - size.X) / 2, (rows * cellSize - size.Y) / 2);
+                _spriteBatch.DrawString(font, msg, pos, Color.Yellow);
+            }
 
             _spriteBatch.End();
             base.Draw(gameTime);
